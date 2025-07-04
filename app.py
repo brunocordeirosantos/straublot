b3ce429ee9d34ac7ac72e6198482f71e09f4f288
brunocordeirosantos <134300297+brunocordeirosantos@users.noreply.github.com>
2025-07-03 22:32:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -720,8 +720,6 @@ def render_dashboard_caixa(spreadsheet):
         tipos_cheques_validos = ["Cheque à Vista", "Cheque Pré-datado", "Cheque com Taxa Manual"]
         total_cheques = df_operacoes[df_operacoes['Tipo_Operacao'].isin(tipos_cheques_validos)]['Valor_Liquido'].sum()
         total_saques = df_operacoes[df_operacoes['Tipo_Operacao'].str.contains("Saque", case=False, na=False)]['Valor_Liquido'].sum()
-        total_saques += total_cheques
-    total_saques += total_cheques
         
         # Converter colunas numéricas com tratamento de erro
         for col in ["Valor_Bruto", "Valor_Liquido", "Taxa_Cliente", "Taxa_Banco", "Lucro"]:
