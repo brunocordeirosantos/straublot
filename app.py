90ae62a5024883b55681c0d9d7852b1193446680
brunocordeirosantos <134300297+brunocordeirosantos@users.noreply.github.com>
2025-07-06 22:43:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import streamlit as st
 import gspread
+
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import json
-import os
-from datetime import datetime, date, timedelta
-import plotly.express as px
-import plotly.graph_objects as go
-from decimal import Decimal, ROUND_HALF_UP, InvalidOperation
-import hashlib
+                "type": st.secrets["gcp_service_account"]["type"],
+                "project_id": st.secrets["gcp_service_account"]["project_id"],
+                "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+                "private_key": st.secrets["gcp_service_account"]["private_key"],
+                "client_email": st.secrets["gcp_service_account"]["client_email"],
+                "client_id": st.secrets["gcp_service_account"]["client_id"],
+                "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+                "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+                "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+                "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
+        client = gspread.authorize(creds)
+
+
+
+
 
 #  Importar pytz com tratamento de erro
 try:
